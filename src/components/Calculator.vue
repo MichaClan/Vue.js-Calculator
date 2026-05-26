6d8fd40a987e508eab61ee1e4f9e54756b83f7a5
MichaClan <302946432@student.rocmondriaan.nl>
2026-05-26 08:50:21 +0200

    Vue uses a specific structure that of HTML. Changed Calculator component name to TheCalculator and added a line there to disable eslint to remove errors and run serve

--- src/components/Calculator.vue
+++
@@ -1,5 +0,0 @@
-const eersteGetal = ref(0);
-const tweedeGetal = ref(0);
-const resultaat = ref(0);
-const operator = ref(null)
-const display = ref("0")
---
+++ src/components/TheCalculator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{ display }}</p>
+  </div>
+</template>
+
+<script setup>
+/* eslint-disable */
+import { ref } from 'vue'
+
+const eersteGetal = ref(0)
+const tweedeGetal = ref(0)
+const resultaat = ref(0)
+const operator = ref(null)
+const display = ref("0")
+</script>
+
+<style>
+</style>
