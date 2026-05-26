70af8940393b6d48bb79062f0f570b1316432d9f
MichaClan <302946432@student.rocmondriaan.nl>
2026-05-26 08:57:06 +0200

    Buttons that add numbers to calculates and a function that changes the 0 into the next added number

--- src/components/TheCalculator.vue
+++ src/components/TheCalculator.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
+   <div>
     <p>{{ display }}</p>
+    <button @click="drukOpCijfer('1')">1</button>
+    <button @click="drukOpCijfer('2')">2</button>
+    <button @click="drukOpCijfer('3')">3</button>
   </div>
 </template>
 
@@ -13,6 +16,14 @@ const tweedeGetal = ref(0)
 const resultaat = ref(0)
 const operator = ref(null)
 const display = ref("0")
+
+function drukOpCijfer(cijfer) {
+  if (display.value === "0") {
+    display.value = cijfer
+  } else {
+    display.value = display.value + cijfer
+  }
+}
 </script>
 
 <style>
